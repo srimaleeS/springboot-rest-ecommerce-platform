002e4c2a1d359882677c4a49d972c6ac10855399
Srimalee_S <srimalee.ss@gmail.com>
2025-07-11 16:12:49 +0200

    Creating dynamic routes + Setting HTTP status codes

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -4,6 +4,8 @@ import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,15 @@ public class UserController {
         return userRepository.findAll();
     }
 
+    //ResponseEntity is a Spring class that represents the entire HTTP response, including status code, headers, and body
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id){ //PathVariable will allow to pass an ID dynamically to the route
-        return userRepository.findById(id).orElse(null);
+    public ResponseEntity<User> getUser(@PathVariable Long id){ //PathVariable will allow to pass an ID dynamically to the route
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build(); //Return 404 Not Found if user is not found
+        }
+//        return new ResponseEntity<>(user,HttpStatus.OK);
+        return ResponseEntity.ok(user); //Return 200 OK with the user object if found
     }
 }
