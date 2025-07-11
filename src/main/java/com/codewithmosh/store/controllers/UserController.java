c4bb27d6fe88be30b3d971de70e7e52b48528599
Srimalee_S <srimalee.ss@gmail.com>
2025-07-11 14:19:48 +0200

    Creating dynamic routes

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -3,17 +3,26 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //Restfull API to fetch users from the db and return them in JSON format
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id){ //PathVariable will allow to pass an ID dynamically to the route
+        return userRepository.findById(id).orElse(null);
     }
 }
